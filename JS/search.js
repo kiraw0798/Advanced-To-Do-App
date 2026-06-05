71c1d9f4b2869f8108fc5ae6341c9583dbbb5254
Kira <111601547+kiraw0798@users.noreply.github.com>
2026-06-05 21:05:05 +0300

    Implement task search feature
    Add search functionality to filter tasks based on user input.

--- JS/search.js
+++ JS/search.js
@@ -1,1 +1,22 @@
+const searchInput = document.querySelector('.search');
+const searchBtn = document.querySelector('.searchBtn');
+
+
+searchInput.addEventListener('input' , handleSearch);
+
+
+
+
+
+function handleSearch(){
+    const query = searchInput.value.toLowerCase();
+
+    let allTasks = JSON.parse(localStorage.getItem('myTasks'));
+
+    const filteredTasks = allTasks.filter((task) => task.formTaskName.toLowerCase().includes(query) || task.formTaskDesc.toLowerCase().includes(query));
+
+    renderTasks(filteredTasks);
+
+
+}
 
